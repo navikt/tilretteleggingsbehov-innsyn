96940de199b958a8aeef51b35e06a3bffe075df8
Kjetil Svalestuen <kjetil.svalestuen@nav.no>
2020-02-04 15:58:08 +0100

    Injiser dekoratør i server-fil

--- server/server.js
+++ server/server.js
@@ -1,14 +1,39 @@
 const path = require('path');
 const express = require('express');
+const hentDekoratør = require('./dekoratør');
+const mustacheExpress = require('mustache-express');
+
+const buildPath = path.join(__dirname, '../build');
 const app = express();
+
+app.engine('html', mustacheExpress());
+app.set('view engine', 'mustache');
+app.set('views', buildPath);
 
 const PORT = 3000;
 const BASE_PATH = '/tilretteleggingsbehov-innsyn';
 
-const buildPath = path.join(__dirname, '../build');
+const renderAppMedDekoratør = dekoratør =>
+    new Promise((resolve, reject) => {
+        console.log('Injiserer dekoratør i HTML-template ...');
+        app.render('index.html', dekoratør, (err, html) => {
+            if (err) {
+                reject(err);
+            } else {
+                console.log('> Suksess!\n');
+                resolve(html);
+            }
+        });
+    });
 
-const startServer = () => {
-    app.use(BASE_PATH, express.static(buildPath));
+const startServer = html => {
+    const ignorerIndex = { index: false };
+
+    app.use(BASE_PATH, express.static(buildPath, ignorerIndex));
+
+    app.get(`${BASE_PATH}/*`, (req, res) => {
+        res.send(html);
+    });
 
     app.get(`${BASE_PATH}/internal/isAlive`, (req, res) => res.sendStatus(200));
     app.get(`${BASE_PATH}/internal/isReady`, (req, res) => res.sendStatus(200));
@@ -18,4 +43,11 @@ const startServer = () => {
     });
 };
 
-startServer();
+const behandleFeil = feil => error => {
+    console.error(`${feil}:`, error);
+    process.exit(1);
+};
+
+hentDekoratør()
+    .then(renderAppMedDekoratør, behandleFeil('Kunne ikke hente dekoratør'))
+    .then(startServer, behandleFeil('Kunne ikke rendre app'));
