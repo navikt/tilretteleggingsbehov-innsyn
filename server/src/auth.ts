f6a83e42539e5bba126fc1cbe5702e9e50a63cc6
Mads Lee Giil <mads.lee.giil@nav.no>
2021-04-20 08:14:38 +0200

    WIP: Legg til try catch

--- server/src/auth.ts
+++ server/src/auth.ts
@@ -11,18 +11,23 @@ let idportenClient: Client;
 
 export const init = async () => {
     const idportenIssuer = await Issuer.discover(discoveryUrl);
-    idportenClient = new idportenIssuer.Client(
-        {
-            client_id: clientId,
-            token_endpoint_auth_method: 'private_key_jwt',
-            token_endpoint_auth_signing_alg: 'RS256',
-            redirect_uris: [redirectUri, 'http://localhost:3000/oauth2/callback'],
-            response_types: ['code'],
-        },
-        {
-            keys: [JSON.parse(jwk)],
-        }
-    );
+
+    try {
+        idportenClient = new idportenIssuer.Client(
+            {
+                client_id: clientId,
+                token_endpoint_auth_method: 'private_key_jwt',
+                token_endpoint_auth_signing_alg: 'RS256',
+                redirect_uris: [redirectUri, 'http://localhost:3000/oauth2/callback'],
+                response_types: ['code'],
+            },
+            {
+                keys: [JSON.parse(jwk)],
+            }
+        );
+    } catch (err) {
+        console.log('Kunne ikke opprette idportenClient');
+    }
 };
 
 export const authUrl = (session: Session, nonce: string, state: string) =>
