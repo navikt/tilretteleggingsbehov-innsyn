4e6c90477a92db9a25a4c92885390d4c719dca4f
Kjetil Svalestuen <kjetil.svalestuen@nav.no>
2021-04-27 10:06:01 +0200

    Logg med JSON

--- server/src/logging.ts
+++ server/src/logging.ts
@@ -1,7 +1,7 @@
 import winston from 'winston';
 
 export const log = winston.createLogger({
-    format: winston.format.logstash(),
+    format: winston.format.json(),
     level: 'info',
     transports: [new winston.transports.Console()],
     exceptionHandlers: [new winston.transports.Console()],
