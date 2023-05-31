08cca6c79c5f29595a7798982e239ca11a53f44a
Mads Lee Giil <mads.lee.giil@nav.no>
2023-05-31 12:08:20 +0200

    Fjern visning av tilretteleggingsbehov

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,11 @@
-import React, { useEffect, useState } from 'react';
-import { hentTilretteleggingsbehov, Respons, Status } from './api/api';
+import React from 'react';
 import { ReactComponent as Tavleikon } from './tavleikon/tavleikon.svg';
 import Brødsmulesti from './brødsmulesti/Brødsmulesti';
-import Informasjon from './informasjon/Informasjon';
 import Tilbake from './tilbake/Tilbake';
-import Visning from './visning/Visning';
 import { Heading } from '@navikt/ds-react';
 import css from './App.module.css';
 
 const App = () => {
-    const [respons, setRespons] = useState<Respons>({
-        status: Status.IkkeLastet,
-    });
-
-    useEffect(() => {
-        const hent = async () => {
-            setRespons({
-                status: Status.LasterInn,
-            });
-
-            setRespons(await hentTilretteleggingsbehov());
-        };
-
-        hent();
-    }, []);
-
-    useEffect(() => {
-        if (respons.status === Status.Feil && respons.statusKode === 401) {
-            window.location.replace('./redirect-til-login');
-        }
-    }, [respons]);
-
     return (
         <div className={css.app}>
             <Brødsmulesti />
@@ -44,9 +19,10 @@ const App = () => {
                 </header>
                 <span className={css.spacer} />
             </div>
+
             <main className={css.main}>
-                <Informasjon respons={respons} />
-                {respons.status === Status.Suksess && <Visning kandidat={respons.kandidat} />}
+                NAV registrerer ikke lenger tilretteleggingsbehov i forbindelse med arbeidsrettet
+                oppfølging. Informasjon som tidligere har vært registrert er nå slettet.
             </main>
         </div>
     );
